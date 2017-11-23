9162948cfcdf03e8945047d2a4b23ced929eb91d
Sasa Ristic <sasja.ristic@gmail.com>
2017-11-23 14:27:13 +0100

    Spread Operator

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,7 +1,27 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers } from "redux";
+import uuid from "uuid";
 
 // ADD_EXPENSES
-// REMOVE_EXPENSES
+const addExpenses = (
+  { description = "", note = "", amount = 0, createdAt = 0 } = {}
+) => ({
+  type: "ADD_EXPENSE",
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+// REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
+
 // EDIT_EXPENSES
 // SET_TEXT_FILTER
 // SORT_BY_DATE
@@ -9,13 +29,20 @@ import { createStore, combineReducers } from 'redux';
 // SET_START_DATE
 // SET_END_DATE
 
-
 // EXPENSES Reducers
 
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case "ADD_EXPENSE":
+      return [
+        ...state,
+        action.expense
+      ];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id);
+      break;
     default:
       return state;
   }
@@ -24,8 +51,8 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
 // Filters Reducer
 // text => '', sortBy => 'date', startDate => undefined, endDate => undefined
 const filtersReducerDefaultState = {
-  text: '',
-  sortBy: 'date',
+  text: "",
+  sortBy: "date",
   startDate: undefined,
   endDate: undefined
 };
@@ -37,8 +64,6 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
   }
 };
 
-
-
 // Store creation
 const store = createStore(
   combineReducers({
@@ -47,19 +72,28 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpenses({ description: "Rent", amount: 100 }));
+const expenseTwo = store.dispatch(addExpenses({ description: "Covfefe", amount: 300 }));
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 const demoState = {
-  expenses: [{
-    id: 'pusikurac',
-    description: 'January Rent',
-    note: 'This was the final payment for that adress',
-    amount: 54500,
-    createdAt: 0
-  }],
+  expenses: [
+    {
+      id: "pusikurac",
+      description: "January Rent",
+      note: "This was the final payment for that adress",
+      amount: 54500,
+      createdAt: 0
+    }
+  ],
   filters: {
-    text: 'rent',
-    sortBy: 'amount', // date or amount
+    text: "rent",
+    sortBy: "amount", // date or amount
     startDate: undefined,
     endDate: undefined
   }
