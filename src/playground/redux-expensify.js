c4eb0a6c0c159311518d1a354314f24715bfea03
Sasa Ristic <sasja.ristic@gmail.com>
2017-11-23 10:51:02 +0100

    Combine Reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,54 @@
 import { createStore, combineReducers } from 'redux';
 
+// ADD_EXPENSES
+// REMOVE_EXPENSES
+// EDIT_EXPENSES
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+
+// EXPENSES Reducers
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+// Filters Reducer
+// text => '', sortBy => 'date', startDate => undefined, endDate => undefined
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+
+
+// Store creation
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+
+console.log(store.getState());
+
 const demoState = {
   expenses: [{
     id: 'pusikurac',
