c31627030ec0dcfaf1e45e3387fa214591cf41fd
Sasa Ristic <sasja.ristic@gmail.com>
2017-11-24 14:00:20 +0100

    Higher Order Components

---
+++ src/playground/hoc.js
@@ -0,0 +1,43 @@
+// Higher Order Component (HOC) - A component that renders another component
+// Reuse Code
+// Render Hijacking
+// Prop Manipulation
+// Abstract State
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Info = props => (
+  <div>
+    <h1>Info</h1>
+    <p>The info is: {props.info}</p>
+  </div>
+);
+
+const withAdminWarning = WrappedComponent => {
+  return props => (
+    <div>
+      {props.isAdmin && <p>this is privete info. Please don't share!!</p>}
+      <WrappedComponent {...props} />
+    </div>
+  );
+};
+
+// requireAuthentication
+const requireAuthentication = WrappedComponent => {
+  return props => (
+    <div>
+      { props.isAuthenticated ? (
+        <WrappedComponent {...props} />
+      ) : (
+        <p>Please Login to view the Info!!</p>
+      ) }
+    </div>
+  );
+};
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={true} info="There are the details" />,document.getElementById("app"));
+ReactDOM.render(<AdminInfo isAuthenticated={true} info="There are the details" />,document.getElementById("app"));
