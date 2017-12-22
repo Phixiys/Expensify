fb8c56655a4986a415ddb89633361f2e4aaa567a
Sasa <sasja.ristic@gmail.com>
2017-12-22 09:47:01 +0100

    End of Testing module

--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -2,28 +2,28 @@ import moment from 'moment';
 
 export default [
   {
-    id: "1",
-    description: "Gum",
-    note: "",
+    id: '1',
+    description: 'Gum',
+    note: '',
     amount: 195,
     createdAt: 0
   },
   {
-    id: "2",
-    description: "Rent",
-    note: "",
+    id: '2',
+    description: 'Rent',
+    note: '',
     amount: 109500,
     createdAt: moment(0)
-      .subtract(4, "days")
+      .subtract(4, 'days')
       .valueOf()
   },
   {
-    id: "3",
-    description: "Credit Card",
-    note: "",
+    id: '3',
+    description: 'Credit Card',
+    note: '',
     amount: 4500,
     createdAt: moment(0)
-      .add(4, "days")
+      .add(4, 'days')
       .valueOf()
   }
 ];
