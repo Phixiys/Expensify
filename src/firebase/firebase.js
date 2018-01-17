0b8106d323c0cb610025e128067aa1f11c0fcfca
Sasa <sasja.ristic@gmail.com>
2018-01-17 13:23:29 +0100

    Firebase enviroment variables setup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,64 +1,66 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyBe6tiUic9sH0ZrBJo3bOpvytxPM_Ia-mo',
-  authDomain: 'expensify-b6502.firebaseapp.com',
-  databaseURL: 'https://expensify-b6502.firebaseio.com',
-  projectId: 'expensify-b6502',
-  storageBucket: 'expensify-b6502.appspot.com',
-  messagingSenderId: '429333778314'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
 
-// child_removed
-database.ref('expenses').on('child_removed', snapshot => {
-  console.log(snapshot.key, snapshot.val());
-});
+export { firebase, database as default };
 
-// child_changed
-database.ref('expenses').on('child_changed', snapshot => {
-  console.log(snapshot.key, snapshot.val());
-});
-
-// child_added
-database.ref('expenses').on('child_added', snapshot => {
-  console.log(snapshot.key, snapshot.val());
-});
-
-// database.ref('expenses')
-//   .once('value')
-//   .then(snapshot => {
-//     const expenses = [];
-
-//     snapshot.forEach(childSnapshot => {
-//       expenses.push({
-//         id: childSnapshot.key,
-//         ...childSnapshot.val()
-//       });
-//     });
-
-//     console.log(expenses);
-//   });
-
-// database.ref('expenses').on('value', snapshot => {
-//   const expenses = [];
-
-//   snapshot.forEach(childSnapshot => {
-//     expenses.push({
-//       id: childSnapshot.key,
-//       ...childSnapshot.val()
-//     });
-//   });
-
-//   console.log(expenses);
+// // child_removed
+// database.ref('expenses').on('child_removed', snapshot => {
+//   console.log(snapshot.key, snapshot.val());
 // });
 
-// database.ref('expenses').push({
-//   description: 'Rent',
-//   note: '',
-//   amount: 109500,
-//   createdAt: 3248764923743
+// // child_changed
+// database.ref('expenses').on('child_changed', snapshot => {
+//   console.log(snapshot.key, snapshot.val());
 // });
+
+// // child_added
+// database.ref('expenses').on('child_added', snapshot => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // database.ref('expenses')
+// //   .once('value')
+// //   .then(snapshot => {
+// //     const expenses = [];
+
+// //     snapshot.forEach(childSnapshot => {
+// //       expenses.push({
+// //         id: childSnapshot.key,
+// //         ...childSnapshot.val()
+// //       });
+// //     });
+
+// //     console.log(expenses);
+// //   });
+
+// // database.ref('expenses').on('value', snapshot => {
+// //   const expenses = [];
+
+// //   snapshot.forEach(childSnapshot => {
+// //     expenses.push({
+// //       id: childSnapshot.key,
+// //       ...childSnapshot.val()
+// //     });
+// //   });
+
+// //   console.log(expenses);
+// // });
+
+// // database.ref('expenses').push({
+// //   description: 'Rent',
+// //   note: '',
+// //   amount: 109500,
+// //   createdAt: 3248764923743
+// // });
